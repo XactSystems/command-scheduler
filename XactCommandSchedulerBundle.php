20d806f0237be55a485e9d1c4bf38caaacd52307
Ian Foulds <ianfoulds@x-act.co.uk>
2020-07-14 11:21:32 +0100

    Create XactCommandSchedulerBundle.php

---
+++ XactCommandSchedulerBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Xact\JobScheduler;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Xact command scheduler bundle
+ */
+class XactCommandSchedulerBundle extends Bundle
+{
+}
